119b494e345dbb6fca8504a2fbd4387d760bdf9a
Nathan-Brauning <nathanbrauning@gmail.com>
2024-02-17 16:39:05 -0500

    Got yfinance to work and print dictionary.

--- Nathan.py
+++ Nathan.py
@@ -1,1 +1,24 @@
-print("hello")
+
+
+import yfinance as yf
+from datetime import datetime, timedelta
+
+x_days_ago = 14
+
+end_date = datetime.now() 
+
+start_date = end_date - timedelta(days=x_days_ago)
+
+print(end_date)
+
+print(start_date)
+
+#Replace "AMZN" with the stock input
+stock = yf.Ticker("AMZN")
+
+stock_history = stock.history(start=start_date, end=end_date)
+
+#Doesn't work now? ;(
+print(stock_history)
+
+
