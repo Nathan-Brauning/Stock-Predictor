7729386053f30d9069f8a5238438847196d181a2
Nathan-Brauning <nathanbrauning@gmail.com>
2024-02-18 10:00:31 -0500

    Webscraping start js.

--- frontend.js
+++ frontend.js
@@ -1,6 +1,13 @@
-var statistics = require('statistics.py')
+var statistics = require('simple-statistics');
+
+var vantage = require('alphavantage')
 
 function stockStats(daysAgo, ticker) {
     //Imports
-    
+    var endDate = Date.now();
+    var startDate = Date.now() - daysAgo;
+
+    var stock = vantage()
 }
+
+
